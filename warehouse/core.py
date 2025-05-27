bac9f8e887aa4ea2a8019e366a69db457b04eadf
Balthazar <baldasketchy@hotmail.com>
2025-05-27 20:08:05 +1000

    Upgrade store_snippet to save snippets in JSON file

--- warehouse/core.py
+++ warehouse/core.py
@@ -1,3 +1,30 @@
+import json
+import os
+
+def store_snippet(name, code, language, filename="snippets.json"):
+    snippet = {
+        "name": name,
+        "code": code,
+        "language": language
+    }
+
+    # Create the file if it doesn't exist
+    if not os.path.exists(filename):
+        with open(filename, "w") as f:
+            json.dump([], f)
+
+    # Load existing snippets
+    with open(filename, "r") as f:
+        snippets = json.load(f)
+
+    # Add the new one
+    snippets.append(snippet)
+
+    # Write back to file
+    with open(filename, "w") as f:
+        json.dump(snippets, f, indent=2)
+
+    return snippet
 def store_snippet(name, code, language):
     return {
         "name": name,
